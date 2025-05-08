dd4a2152c249ccb11c4f29aa2ef9a8dd7e30df16
JimenezAlejandro <17188622+JimenezAlejandro@users.noreply.github.com>
2025-05-07 23:11:15 -0700

    Add asteroid.py, asteroidfield.py, and implement collision

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+import sys
 
 
 def main():
@@ -13,7 +16,11 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
@@ -22,6 +29,10 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
+        for asteroid in asteroids:
+            if asteroid.overlaps(player):
+                print(f"Game over!")
+                sys.exit(0)
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
